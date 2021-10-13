b09134a657fe77738a5f735440d5e136450faa13
Danilo Fontes <slipdan@hotmail.com>
2021-10-13 12:32:03 -0300

    Create TextField and add Logo

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import TextField, { Input } from '@material/react-text-field'
+import logo from '../../assets/logo.svg'
 
-import { Container } from "./style";
+import { Container, Search } from "./style";
 
-const Home = () => <Container>Hello World!!!</Container>;
+const Home = () => {
+    const [inputValue, setInputValue] = useState('');
+    return (
+    <Container>
+        <Search>
+            <img src={logo} alt="Logo do Restaurante Search" />
+            <TextField
+                label='Pesquisar'
+                outlined
+                //  onTrailingIconSelect={{}) => this.setState({value: ''})}
+                // trailingIcon={<MaterialIcon role="button" icon="delete"/>}
+            >
+            <Input
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)} />
+            </TextField>
+        </Search>
+    </Container>
+    );
+};
 
 export default Home;
